5e3529ad135a7be78e4462d6b932b8ad1d592b7c
Harsha TR <63353354+trharsha0603@users.noreply.github.com>
2021-08-31 09:23:50 +0530

    Update index.js

--- test-app/src/pages/index.js
+++ test-app/src/pages/index.js
@@ -13,6 +13,10 @@ import appearances from './appearances';
 import changing_user_id from './changing_user_id';
 import terms_and_policy from './terms_and_policy';
 import change_email_using_mobile from './change_email_using_mobile';
+import change_userid from './change_userid';
+import verify_email from './verify_email';
+import verify_number from './verify_number';
+import changing_mobile_number from './changing_mobile_number';
 
 const pages = () => {
     return(
@@ -23,6 +27,10 @@ const pages = () => {
             <Route path="/changing_user_id" component= {changing_user_id} />
             <Route path="/terms_and_policy" component= {terms_and_policy} />
             <Route path="/change_email_using_mobile" component= {change_email_using_mobile} />
+            <Route path="/change_userid" component= {change_userid} />
+            <Route path="/verify_email" component= {verify_email} />
+            <Route path="/verify_number" component= {verify_number} />
+            <Route path="/changing_mobile_number" component= {changing_mobile_number} />
         </Router>
     )
 }
