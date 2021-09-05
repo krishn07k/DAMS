87b621b9a4aa7548baaf7ed1e6673fe16ab99e44
Jothilaxmi <71388962+Jothilaxmi@users.noreply.github.com>
2021-09-06 00:25:54 +0530

    Add files via upload

---
+++ test-app/src/pages/account_settings.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './account_settings.css';
+
+const account_settings = () => {
+    return(
+        <body className="acset_body">
+            <div class="acset_main_box">
+
+            <div class="acset_title">
+              Account settings
+            </div>
+
+            <div class="acset_subtitle1">
+              User ID
+              <div class="acset_id_icon">
+                     <img src="icons/id.png" alt="id_icon" class="acset_id_icon_image" />
+              </div>
+            </div>
+
+            <div class="acset_subtitle2">
+              Password 
+              <div class="acset_lock_icon">
+                     <img src="icons/lock_icon.png" alt="lock_icon" class="acset_lock_icon_image" />
+              </div>
+            </div>
+
+            <div class="acset_subtitle3">
+              Mobile number
+              <div class="acset_phone_icon">
+                     <img src="icons/phone.png" alt="phone_icon" class="acset_phone_icon_image" />
+              </div>
+            </div>
+
+            <div class="acset_subtitle4">
+              Delete number
+               <div class="acset_deleteaccount_icon">
+                     <img src="icons/deleteaccount.png" alt="deleteaccount_icon" class="acset_deleteaccount_icon_image" />
+               </div>
+            </div>
+
+            
+
+
+            <div class= "acset_reset_button_container1">
+                <button class="acset_reset_button1"></button>
+                
+            </div>
+
+            <div class="acset_reset_button_container2">
+                <button class="acset_reset_button2"></button>
+            </div>
+
+            <div class="acset_reset_button_container3">
+                <button class="acset_reset_button3"> </button>
+            </div>
+
+            <div class="acset_reset_button_container4">
+                <button class="acset_reset_button4"></button>
+            </div>
+
+            <div class="acset_symbol1">
+               {'>'}
+            </div>
+
+            <div class="acset_symbol2">
+               {'>'}
+            </div>
+
+            <div class="acset_symbol3">
+               {'>'}
+            </div>
+
+            <div class="acset_symbol4">
+               {'>'}
+            </div>
+
+           
+
+            </div>
+        </body>
+    );
+}
+
+export default account_settings;
+
+
+
+
