6fce29305fe11b64f4d29d5741d4c0fdd10572b4
Krishnaraj <86999743+krishn07k@users.noreply.github.com>
2021-09-06 21:10:04 +0530

    Update index.js

--- test-app/src/pages/index.js
+++ test-app/src/pages/index.js
@@ -6,7 +6,16 @@ import {
     Route,
     // Link
   } from "react-router-dom";
-
+import forgot_pwd from './forgot_pwd';
+import reset_pwd from './reset_pwd';
+import appearances from './appearances';
+import changing_user_id from './changing_user_id';
+import terms_and_policy from './terms_and_policy';
+import change_email_using_mobile from './change_email_using_mobile';
+import change_userid from './change_userid';
+import verify_email from './verify_email';
+import verify_number from './verify_number';
+import changing_mobile_number from './changing_mobile_number';
 import Signup_dentist from './signup_dentist';
 import Signup_general_user from './signup_general_user';
 import Profile_page from './profile_page';
@@ -21,7 +30,17 @@ import schedular from './schedular';
 const pages = () => {
     return(
         <Router>
-            <Route path="/signup_dentist" exact component= {Signup_dentist} />
+            <Route path="/forgot_pwd" exact component= {forgot_pwd} />
+            <Route path="/reset_pwd" component= {reset_pwd} />
+            <Route path="/appearances" component= {appearances} />
+            <Route path="/changing_user_id" component= {changing_user_id} />
+            <Route path="/terms_and_policy" component= {terms_and_policy} />
+            <Route path="/change_email_using_mobile" component= {change_email_using_mobile} />
+            <Route path="/change_userid" component= {change_userid} />
+            <Route path="/verify_email" component= {verify_email} />
+            <Route path="/verify_number" component= {verify_number} />
+            <Route path="/changing_mobile_number" component= {changing_mobile_number} />            
+            <Route path="/signup_dentist" component= {Signup_dentist} />
             <Route path="/signup_general_user" component= {Signup_general_user} />
             <Route path="/license" component= {License_page} />
             <Route path="/notifications" component= {Notifications_page} />
